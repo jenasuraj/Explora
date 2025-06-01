691333425b239c2e7b56b8b8ab3e311b08bb74a4
jenasuraj <jenasuraj742@gmail.com>
2025-06-01 22:08:24 +0530

    rag added

---
+++ README.md
@@ -0,0 +1,68 @@
+![My Image](./assets/screenshot.png)
+
+
+<p align="center">
+  <img src="assets/screenshot.png" alt="Banner" width="600" />
+</p>
+
+<h1 align="center">🚀 Ai travel planner</h1>
+
+<p align="center">
+  Ai travel planner <br/>
+  A system that finds one of the best places around the globe
+</p>
+
+---
+
+## 🧠 About
+
+This project uses LangChain to intelligently suggest the best travel destinations based on user preferences and travel days. It integrates Retrieval-Augmented Generation (RAG) to fetch and display relevant website data, providing rich contextual details about each place. Powered by tools like SQL, Tavily, and modern web technologies, the app delivers smart, day-specific travel planning with real-time insights.
+
+---
+
+## 🛠️ Tech Stack
+
+| Frontend |     Backend     | Database | Styling   |         Ai          |
+|----------|-----------------|----------|-----------|---------------------|
+| Next.js  | Express,fastapi | MySQL    | Tailwind  | langchain,langgraph |
+
+---
+
+## ✨ Features
+
+- 📋 Dynamic routing with Next.js
+- 🛠️ RAG implementation
+- 💾 MySQL database integration
+- 🔐 Secure API endpoints
+- 💻 Fully responsive design
+- ✨ Amazing plan by ai
+
+---
+
+## 🚀 Getting Started
+# Clone the repo
+git clone git@github.com:jenasuraj/Ai-TravelPlanner-app.git
+
+
+# required dependencies front end
+npx create-next-app@latest next-frontend
+cd next-frontend
+npm run dev
+
+
+# required dependencies for Express  
+mkdir express-backend
+cd express-backend
+npm init -y
+npm install express cors dotenv
+node index.js
+
+
+# required dependencies for fastapi
+python -m venv venv
+source venv/bin/activate 
+pip install fastapi uvicorn
+uvicorn main:app --reload
+Your FastAPI server runs at http://127.0.0.1:8000
+
+```
--- backend/services/langgraph_text.py
+++ backend/services/langgraph_text.py
@@ -6,15 +6,33 @@ from models.user import text_llm
 from langgraph.prebuilt import tools_condition
 from langgraph.prebuilt import ToolNode
 from langchain_tavily import TavilySearch
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage,AIMessage
 from dotenv import load_dotenv
 from tools.sql import sql as sql_tool
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
+
+
+loader = PyPDFLoader(r"C:\Users\LENOVO\Downloads\Travel_App_RAG_Report_Suraj.pdf")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size =100, chunk_overlap = 10)
+spliited_text = text_splitter.split_documents(documents)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+vector_store = InMemoryVectorStore(embeddings)
+ids = vector_store.add_documents(documents=spliited_text)
+print("data added to v-db")
+retriever = vector_store.as_retriever(search_kwargs={"k": 2})  
+
 
 tavily_tool = TavilySearch(max_results=2)
 tools = [tavily_tool,sql_tool]
 llm_with_tools = text_llm.bind_tools(tools)
+
 
 
 ALLOWED_TOPICS = ALLOWED_TOPICS = [
@@ -52,9 +70,23 @@ def chatbot(state: State):
 
 
 def rag(state: State):
-    print("\n\nOn ending stuff, no tools needed.")
-    return {
-        "messages": state["messages"]
+    #print("the state is",state["messages"])
+    user_query = state["messages"][0].content
+    rag_response = retriever.invoke(state["messages"][0].content)
+    rag_response_modified ="\n\n".join([i.page_content for i in rag_response])
+    print("rag response is --->",rag_response_modified)
+    tool_or_llm_response = state["messages"][-1].content 
+    system_prompt = (
+        "You are a helpful assistant. Use the following context to answer the question:\n\n"
+        f"The rag response is :{rag_response_modified}\n\n while the existing response by tools/llm is: {tool_or_llm_response}"
+        "Now answer the user's question."
+     )
+    result = text_llm.invoke([
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content":user_query }
+    ])
+    return{
+        "messages":state["messages"]+[AIMessage(content=result.content)]
     }
 
 
@@ -62,7 +94,6 @@ graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("tools", ToolNode(tools))
 graph_builder.add_node("rag", rag)
-
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_conditional_edges("chatbot", tools_condition, {
     "tools": "tools",
