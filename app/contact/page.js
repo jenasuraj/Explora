9277b647dfe8968c45f9aa6f78d4388467f048c0
jenasuraj <jenasuraj742@gmail.com>
2025-07-27 12:30:31 +0530

    (chore): add ESLint configuration

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='min-h-screen flex flex-col justify-center items-center'>
-     <p className='text-2xl font-mono'> Under-constructing , sorry for the inconvenience</p>
+     <p className='text-2xl font-mono'> Under-constructing sorry for the inconvenience</p>
     <p className='text-9xl font-extrabold text-red-500'>404</p>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import Section1 from "@/components/home/Section1";
-import Section2 from "@/components/home/Section2";
-import Section3 from "@/components/home/Section3";
-import Section4 from "@/components/home/Section4";
+import Section1 from "@/components/home/Section1"
+import Section2 from "@/components/home/Section2"
+import Section3 from "@/components/home/Section3"
+import Section4 from "@/components/home/Section4"
 
 
 export const metadata = {
