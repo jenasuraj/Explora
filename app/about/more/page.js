f50f9220569481b73a9479ff3ef67b783aac5f6c
jenasuraj <jenasuraj742@gmail.com>
2025-08-19 07:18:08 +0530

    delete: extra form

--- app/about/more/page.js
+++ app/about/more/page.js
@@ -2,18 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 const page = () => {
   return (
-    <div className='flex flex-col justify-center items-center  '>
-
+    <div className='flex flex-col justify-center items-center'>
       <p>Might be interested in some of our other platforms</p>
-      
       <Link href='/services'>
    <button className='mt-10 inline-block bg-black px-6 py-1 text-white rounded-sm cursor-pointer'>Know a bit more about services</button>
       </Link>
-
 <Link href='/contact'>
    <button className='mt-10 inline-block bg-black px-6 py-1 text-white rounded-sm cursor-pointer'>Know a bit more how to contact</button>
 </Link>
-
     </div>
   )
 }
--- app/about/page.js
+++ app/about/page.js
@@ -57,3 +57,4 @@ const AboutPage = () => {
 };
 
 export default AboutPage;
+
