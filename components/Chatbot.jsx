e7faa5d80f08bb7cbd502b14348d6ff22975125a
jenasuraj <jenasuraj742@gmail.com>
2025-09-25 10:19:19 +0530

    fix: added headers to render endpoint

--- components/Chatbot.jsx
+++ components/Chatbot.jsx
@@ -55,7 +55,10 @@ const Chatbot = ({firstResponse,setFirstResponse,finalData,setFinalData}) => {
 
     setErrorMessage('');
     try {
-      const res = await axios.post('https://explora-backend-k0e5.onrender.com/', { mixed_data });
+    const res = await axios.post(
+     'https://explora-backend-k0e5.onrender.com/',
+     { mixed_data },
+     { headers: {'Content-Type': 'application/json',},})
       setFirstResponse(true);
       setFormData(prev => ({ ...prev, description: '' }));
       setFinalData(
